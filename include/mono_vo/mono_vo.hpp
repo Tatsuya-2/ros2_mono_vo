f8f29c1a3323a7065f3d7ffa8eb781c04fa10bdc
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-07-05 07:01:16 +0000

    cleanup

--- include/mono_vo/mono_vo.hpp
+++ include/mono_vo/mono_vo.hpp
@@ -11,41 +11,18 @@
 namespace mono_vo
 {
 
-/**
- * @brief MonoVo class
- */
 class MonoVO : public rclcpp::Node
 {
 public:
-  /**
-   * @brief Constructor
-   *
-   * @param options node options
-   */
   explicit MonoVO(const rclcpp::NodeOptions & options);
 
 private:
-  /**
-   * @brief Sets up subscribers, publishers, etc. to configure the node
-   */
   void setup();
 
-  /**
-   * @brief Processes messages received by a subscriber
-   *
-   * @param msg message
-   */
   void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr & msg);
 
 private:
-  /**
-   * @brief Subscriber
-   */
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
-
-  /**
-   * @brief Publisher
-   */
   rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
 };
 
