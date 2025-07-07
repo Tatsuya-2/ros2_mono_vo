a2a54bd3554a34bc021d0e36fc812bcc12eb9939
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-07-07 06:53:56 +0000

    added logger + better parallax check

--- include/mono_vo/initializer.hpp
+++ include/mono_vo/initializer.hpp
@@ -2,6 +2,7 @@
 
 #include <opencv2/opencv.hpp>
 #include <optional>
+#include <rclcpp/logging.hpp>
 #include <vector>
 
 namespace mono_vo
@@ -16,8 +17,11 @@ struct FrameData
 class Initializer
 {
 public:
-  Initializer()
-  : initalized_(false), distribution_thresh_(0.5f), matcher_(cv::BFMatcher(cv::NORM_HAMMING))
+  Initializer(rclcpp::Logger logger = rclcpp::get_logger("Initializer"))
+  : logger_(logger),
+    initalized_(false),
+    distribution_thresh_(0.5f),
+    matcher_(cv::BFMatcher(cv::NORM_HAMMING))
   {
     orb_det_ = cv::ORB::create(1000);
   }
@@ -30,7 +34,7 @@ public:
     cv::Mat descriptors;
     orb_det_->detectAndCompute(grey_img, cv::Mat(), keypoints, descriptors);
 
-    std::cout << "totals kps: " << keypoints.size() << std::endl;
+    RCLCPP_INFO(logger_, "totals kps: %ld", keypoints.size());
 
     // check distribution in a grid across the image
     cv::Mat grid = cv::Mat::zeros(grey_img.rows / 50, grey_img.cols / 50, CV_8U);
@@ -43,14 +47,46 @@ public:
       }
     }
     auto total_cells = grid.cols * grid.rows;
-    std::cout << "occupied cells: " << occupied_cells << " total cells: " << total_cells
-              << std::endl;
+    RCLCPP_INFO(logger_, "occupied cells: %d total cells: %d", occupied_cells, total_cells);
     auto occupancy = static_cast<float>(occupied_cells) / total_cells;
-    std::cout << "occupancy: " << occupancy << std::endl;
+    RCLCPP_INFO(logger_, "occupancy: %f", occupancy);
     if (occupancy > distribution_thresh_) {
       return FrameData{grey_img, keypoints, descriptors};
     }
     return std::nullopt;
+  }
+
+  bool check_parallax(const std::vector<cv::Point2f> & pts1, const std::vector<cv::Point2f> & pts2)
+  {
+    if (pts1.size() != pts2.size() || pts1.size() < 100) return false;
+
+    // calculate homography
+    std::vector<uchar> inliers_h;
+    cv::findHomography(pts1, pts2, cv::RANSAC, ransac_thresh_h_, inliers_h);
+    int score_h = cv::countNonZero(inliers_h);
+
+    // calculate fundamental
+    std::vector<uchar> inliers_f;
+    cv::findFundamentalMat(pts1, pts2, cv::FM_RANSAC, ransac_thresh_f_, 0.99, inliers_f);
+    int score_f = cv::countNonZero(inliers_f);
+
+    RCLCPP_INFO(logger_, "score h: %d, score f: %d", score_h, score_f);
+    // check 1: min inliers
+    if (static_cast<double>(score_f) / pts1.size() < f_inlier_thresh_) {
+      return false;
+    }
+
+    auto model_score = static_cast<double>(score_h) / static_cast<double>(score_f);
+    RCLCPP_INFO(logger_, "score_h/score_f = %lf", model_score);
+
+    // check 2: Is the Fundamental Matrix a significantly better model?
+    // The ratio score_H / score_F should be low.
+    // A high ratio means Homography explains the data almost as well as Fundamental Matrix.
+    if (model_score > model_score_thresh_) {
+      return false;
+    }
+
+    return true;
   }
 
   void update(const cv::Mat & grey_img, const cv::Matx33d & K)
@@ -58,7 +94,7 @@ public:
     if (!first_frame_.has_value()) {
       first_frame_ = good_first_frame(grey_img);
       if (!first_frame_.has_value()) return;
-      std::cout << "found good first frame" << std::endl;
+      RCLCPP_INFO(logger_, "found good first frame");
     }
 
     std::vector<cv::KeyPoint> keypoints;
@@ -67,7 +103,7 @@ public:
 
     std::vector<std::vector<cv::DMatch>> knn_matches;
     matcher_.knnMatch(first_frame_->descriptors, descriptors, knn_matches, 2);
-    std::cout << "total matches: " << knn_matches.size() << std::endl;
+    RCLCPP_INFO(logger_, "total matches: %ld", knn_matches.size());
 
     std::vector<cv::DMatch> good_matches;
     for (auto & match : knn_matches) {
@@ -76,14 +112,11 @@ public:
       }
     }
 
-    std::cout << "good matches: " << good_matches.size() << std::endl;
+    RCLCPP_INFO(logger_, "good matches: %ld", good_matches.size());
 
     cv::Mat img_matches;
     cv::drawMatches(
       first_frame_->image, first_frame_->keypoints, grey_img, keypoints, good_matches, img_matches);
-
-    cv::imshow("Matches", img_matches);
-    cv::waitKey(0);
 
     std::vector<cv::Point2f> pts1, pts2;
     for (const auto & match : good_matches) {
@@ -91,50 +124,65 @@ public:
       pts2.push_back(keypoints[match.trainIdx].pt);
     }
 
-    std::cout << " K: " << K << std::endl;
-
-    cv::Mat mask;
-    cv::Mat E = cv::findEssentialMat(pts1, pts2, K, cv::RANSAC, 0.999, 1.0, mask);
-
-    if (E.empty()) {
-      std::cout << "Essential matrix estimation failed" << std::endl;
-      return;
-    }
-    std::cout << "Essential matrix estimated\n" << E << std::endl;
-    std::cout << "Mask" << mask << std::endl;
-
-    cv::Mat R, t;
-    int inliers = cv::recoverPose(E, pts1, pts2, K, R, t, mask);
-
-    std::cout << "inliers: " << inliers << std::endl;
-
-    // Check inlier ratio
-    double inlier_ratio = static_cast<double>(inliers) / pts1.size();
-    if (inlier_ratio < 0.3) {
-      std::cout << "Too few inliers: " << inlier_ratio << std::endl;
+    if (!check_parallax(pts1, pts2)) {
+      RCLCPP_WARN(logger_, "Parallax check failed");
+      cv::imshow("Matches", img_matches);
+      cv::waitKey(1);
       return;
     }
 
-    std::cout << "Initial pose estimated" << std::endl;
+    RCLCPP_INFO(logger_, "Parallax check passed");
+    cv::imshow("Matches", img_matches);
+    cv::waitKey(0);
 
-    // Triangulate 3D points
-    cv::Mat P1 = K * cv::Mat::eye(3, 4, CV_64F);  // First camera [I|0]
-    cv::Mat P2 =
-      K * (cv::Mat_<double>(3, 4) << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2),
-           t.at<double>(0, 0), R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2),
-           t.at<double>(1, 0), R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2),
-           t.at<double>(2, 0));
+    // RCLCPP_INFO(logger_, " K: " << K);
 
-    cv::Mat points4D;
-    cv::triangulatePoints(P1, P2, pts1, pts2, points4D);
+    // cv::Mat mask;
+    // cv::Mat E = cv::findEssentialMat(pts1, pts2, K, cv::RANSAC, 0.999, 1.0, mask);
 
-    std::cout << "3D points estimated" << std::endl;
+    // if (E.empty()) {
+    //   RCLCPP_INFO(logger_, "Essential matrix estimation failed");
+    //   return;
+    // }
+    // RCLCPP_INFO(logger_, "Essential matrix estimated\n" << E);
+
+    // cv::Mat R, t;
+    // int inliers = cv::recoverPose(E, pts1, pts2, K, R, t, mask);
+
+    // RCLCPP_INFO(logger_, "inliers: " << inliers);
+
+    // // Check inlier ratio
+    // double inlier_ratio = static_cast<double>(inliers) / pts1.size();
+    // if (inlier_ratio < 0.3) {
+    //   RCLCPP_INFO(logger_, "Too few inliers: " << inlier_ratio);
+    //   return;
+    // }
+
+    // RCLCPP_INFO(logger_, "Initial pose estimated");
+
+    // // Triangulate 3D points
+    // cv::Mat P1 = K * cv::Mat::eye(3, 4, CV_64F);  // First camera [I|0]
+    // cv::Mat P2 =
+    //   K * (cv::Mat_<double>(3, 4) << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2),
+    //        t.at<double>(0, 0), R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2),
+    //        t.at<double>(1, 0), R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2),
+    //        t.at<double>(2, 0));
+
+    // cv::Mat points4D;
+    // cv::triangulatePoints(P1, P2, pts1, pts2, points4D);
+
+    // RCLCPP_INFO(logger_, "3D points estimated");
   }
 
 private:
+  rclcpp::Logger logger_;
   bool initalized_;
   std::optional<FrameData> first_frame_;
   float distribution_thresh_;
+  double ransac_thresh_h_ = 2.0;      // px homography RANSAC threshold
+  double ransac_thresh_f_ = 1.0;      // px fundamental RANSAC threshold
+  double f_inlier_thresh_ = 0.5;      // fundamental inlier threshold ratio
+  double model_score_thresh_ = 0.45;  // max H/F ratio
 
   cv::Ptr<cv::ORB> orb_det_;
   cv::BFMatcher matcher_;
