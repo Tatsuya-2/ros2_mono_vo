e6435ef478df11ec495f9512d162f91740ccf89f
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-07-17 05:37:55 +0000

    refactor landmark to class with id setting

--- include/mono_vo/landmark.hpp
+++ include/mono_vo/landmark.hpp
@@ -4,10 +4,20 @@
 
 namespace mono_vo
 {
-struct Landmark
+class Landmark
 {
+public:
+  Landmark(const cv::Point3f & pose_w, const cv::Mat & descriptor)
+  : id(next_id_++), pose_w(pose_w), descriptor(descriptor)
+  {
+  }
   long id;
   cv::Point3f pose_w;  // pose in world frame
   cv::Mat descriptor;
+
+private:
+  static long next_id_;
 };
+
+long Landmark::next_id_ = 0;
 }  // namespace mono_vo
