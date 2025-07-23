f22b3cb4cbce5c98e1570b9315220268096ad01c
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-07-23 06:16:35 +0000

    clean up frame

--- include/mono_vo/frame.hpp
+++ include/mono_vo/frame.hpp
@@ -23,11 +23,32 @@ public:
   std::vector<cv::Point2f> get_points_2d() const
   {
     std::vector<cv::Point2f> points_2d;
-    points_2d.resize(keypoints.size());
-    for (size_t i = 0; i < keypoints.size(); i++) {
-      points_2d[i] = keypoints[i].pt;
+    points_2d.reserve(keypoints.size());
+    for (const auto & keypoint : keypoints) {
+      points_2d.push_back(keypoint.pt);
     }
     return points_2d;
+  }
+
+  void filter_observations_by_mask(const std::vector<uchar> & inlier_mask)
+  {
+    if (keypoints.size() != inlier_mask.size() || descriptors.rows != inlier_mask.size()) {
+      throw std::runtime_error("Inlier mask, keypoints and descriptors must have the same size");
+    }
+    std::vector<cv::KeyPoint> in_kps;
+    std::vector<cv::Mat> in_desc_rows;
+    in_kps.reserve(inlier_mask.size());
+    in_desc_rows.reserve(inlier_mask.size());
+    for (size_t i = 0; i < inlier_mask.size(); ++i) {
+      if (inlier_mask[i]) {
+        in_kps.push_back(keypoints[i]);
+        in_desc_rows.push_back(descriptors.row(i));
+      }
+    }
+    keypoints = in_kps;
+    cv::Mat in_descs;
+    cv::vconcat(in_desc_rows, in_descs);
+    descriptors = in_descs;
   }
 
   long id;
