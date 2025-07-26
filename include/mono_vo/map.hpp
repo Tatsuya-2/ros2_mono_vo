2c8748277719ac759952d331aa0d638110b8ed0c
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-07-26 06:39:47 +0000

    get point correspondences directly from map

--- include/mono_vo/map.hpp
+++ include/mono_vo/map.hpp
@@ -22,15 +22,29 @@ public:
 
   const Landmark & get_landmark(long id) { return landmarks_.at(id); }
 
-  std::vector<cv::Point3f> get_observed_landmark_points(
-    const std::vector<Observation> & observations)
+  /**
+ * Extracts 2D-3D point correspondences from observations.
+ *
+ * This function iterates over a collection of observations, extracting the 2D image
+ * points and corresponding 3D world points from the map's landmarks. The resulting
+ * pairs of 2D-3D points are often used for Perspective-n-Point (PnP) estimation to
+ * determine the camera pose.
+ *
+ * @param observations A vector of observations containing 2D keypoints and associated landmark IDs.
+ * @return A pair of vectors: the first containing 2D points, and the second containing corresponding 3D points.
+ */
+  std::pair<std::vector<cv::Point2f>, std::vector<cv::Point3f>>
+  get_observation_landmark_point_correspondences(const std::vector<Observation> & observations)
   {
+    std::vector<cv::Point2f> points_2d;
     std::vector<cv::Point3f> points_3d;
     points_3d.reserve(observations.size());
+    points_2d.reserve(observations.size());
     for (const auto & obs : observations) {
+      points_2d.push_back(obs.keypoint.pt);
       points_3d.push_back(landmarks_.at(obs.landmark_id).pose_w);
     }
-    return points_3d;
+    return {points_2d, points_3d};
   }
 
   void add_keyframe(const std::shared_ptr<KeyFrame> & keyframe)
